38a78557bbc4ae61084f640721be7b734a92833f
Priya-611 <149269977+Priya-611@users.noreply.github.com>
2025-04-12 15:55:11 +0530

    pyNew.py

--- pyNew.py
+++ pyNew.py
@@ -30,3 +30,25 @@ df[numeric]=df[numeric].apply(pd.to_numeric)
 df1=df[df["Profit"]>=0].copy()
 print(df1)
 
+# merging two sheet
+df1=pd.merge(df1,return_df,on="Order ID",how="left")
+print(df1)
+df1["Returned"].replace(np.nan,"No",inplace=True)
+print(df1)
+print(df1.columns)
+
+#removing outliers 
+
+Q1 = df1["Profit"].quantile(0.25)
+Q3 = df1["Profit"].quantile(0.75)
+IQR = Q3 - Q1
+lower_bound = Q1 - 1.5 * IQR
+upper_bound = Q3 + 1.5 * IQR
+
+df_no_outliers = df1[(df1["Profit"] >= lower_bound) & (df1["Profit"] <= upper_bound)].copy()
+print(df_no_outliers)
+
+print(df_no_outliers.info())
+print(df_no_outliers.describe())
+
+
